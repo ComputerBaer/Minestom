b7e58bfd9637fe75be8a55ffe2c5549e3e636637
themode <themode@outlook.fr>
2022-03-16 20:40:22 +0100

    1.19

--- src/main/java/net/minestom/server/network/packet/server/play/SetChatPreviewPacket.java
+++ src/main/java/net/minestom/server/network/packet/server/play/SetChatPreviewPacket.java
@@ -1,25 +1,23 @@
 package net.minestom.server.network.packet.server.play;
 
-import net.minestom.server.coordinate.Point;
 import net.minestom.server.network.packet.server.ServerPacket;
 import net.minestom.server.network.packet.server.ServerPacketIdentifier;
+import net.minestom.server.utils.binary.BinaryReader;
 import net.minestom.server.utils.binary.BinaryWriter;
 import org.jetbrains.annotations.NotNull;
 
-public class SculkVibrationSignal implements ServerPacket {
-
-    public Point position;
-    public String destinationIdentifier;
-    // TODO 'varies' destination
-    public int arrivalTicks;
+public record SetChatPreviewPacket(boolean enable) implements ServerPacket {
+    public SetChatPreviewPacket(BinaryReader reader) {
+        this(reader.readBoolean());
+    }
 
     @Override
     public void write(@NotNull BinaryWriter writer) {
-
+        writer.writeBoolean(enable);
     }
 
     @Override
     public int getId() {
-        return ServerPacketIdentifier.SCULK_VIBRATION_SIGNAL;
+        return ServerPacketIdentifier.SET_DISPLAY_CHAT_PREVIEW;
     }
 }
